fa3b612042aa098c5c40b2d5c3fecd871a0877c2
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-07-29 09:39:49 -0700

    JavaScript Course
    JavaScript course step 59

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+window.alert("Hello, World");                                                   //Pop-up window with text
+var A = "Hello, ", B = "World!";                                                //Defining two str variables
+document.write(A + B);                                                          //Concatenating two str variables to print on the screen
+var Dad = "Zack", Mom = "Irine", Son = "Joseph-Anthony", Grandma = "Linda";     //Defining str variables
+var Dad = Dad.fontcolor("red");                                                 //Changing var to have red font color
+var Mom = Mom.fontcolor("yellow");                                              //Changing var to have yellow font color
+var Son = Son.fontcolor("blue");                                                //Changing var to have blue font color
+var Grandma = Grandma.fontcolor("green");                                       //Changing var to have green font color
+document.write(Son);                                                            //Calling "Son" var to print on screen
