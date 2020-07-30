8caed3289302cdfdc64ac3f188a8823f1ea83e11
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-07-30 08:05:06 -0700

    JavaScript Course
    Step 64

---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+function Change_to_blue() {
+    var b = "blue";
+    var elem = document.getElementById("ColorChange")
+    elem.style.color = b
+}
+
+function Testing() {
+    var test = "Testing 123";
+    test += " this is still a test.";
+    document.getElementById("String_test").innerHTML = test
+}
